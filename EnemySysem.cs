564471fa9e63c83efbadeb41ee3f32c57c24f8b4
warma5927 <1527776347@qq.com>
2025-10-30 09:53:14 +0800

    Add files via upload

---
+++ EnemySysem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySytsem : MonoBehaviour
+{
+    void OnEnable()
+    {
+        ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer);
+    }
+    void OnDisable()
+    {
+        ActionSystem.DetachPerformer<EnemyTurnGA>();
+    }
+    // Performers
+    private IEnumerator EnemyTurnPerformer(EnemyTurnGA enemyTurnGA)
+    {
+        Debug.Log("Enemy Turn");
+        yield return new WaitForSeconds(2f);
+        Debug.Log("End Enemy Turn");
+    }
+}
